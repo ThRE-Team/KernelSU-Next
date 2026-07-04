5da8e89d1ec2139acc1118f058932121153abd9c
ThRE Team <threteam@gmail.com>
2026-07-04 21:38:41 +0700

    Ultimate

--- kernel/selinux/selinux.c
+++ kernel/selinux/selinux.c
@@ -270,7 +270,12 @@ static void initialize_fake_status(void)
 	if (fake_status) /* double-check after lock */
 		goto out;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 16, 0)
+	struct page *real_page = selinux_kernel_status_page(&selinux_state);
+#else
 	struct page *real_page = selinux_kernel_status_page();
+#endif
+
 	if (!real_page) {
 		pr_warn("ksu_selinux_hide: status_page not exist\n");
 		goto out;
