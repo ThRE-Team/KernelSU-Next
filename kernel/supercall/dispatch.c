6413a97ece30f0ffb1ac851338d29915d141cd3c
ThRE Team <threteam@gmail.com>
2026-09-25 20:29:12 +0700

    update 4

--- kernel/supercall/dispatch.c
+++ kernel/supercall/dispatch.c
@@ -74,6 +74,9 @@ static int do_get_info_legacy(void __user *arg)
 
 #ifdef MODULE
     cmd.flags |= KSU_GET_INFO_FLAG_LKM;
+    if (ksu_bundled) {
+        cmd.flags |= KSU_GET_INFO_FLAG_BUNDLED;
+    }
 #endif
 
 	if (is_manager()) {
